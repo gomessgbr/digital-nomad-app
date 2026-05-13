e5610fe8b71301b9e30b0081c71bfb52f30189c5
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-13 13:54:38 -0300

    refactor: removing unused code and replacing themed components with basic React Native components

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,50 +1,23 @@
-import * as Device from "expo-device";
-import { Platform, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { AnimatedIcon } from "@/components/animated-icon";
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
-import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
 import { Link, useRouter } from "expo-router";
-
-function getDevMenuHint() {
-  if (Platform.OS === "web") {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === "android" ? "cmd+m (or ctrl+m)" : "cmd+d";
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   const router = useRouter();
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <View>
+      <SafeAreaView>
+        <View>
+          <Text>Welcome to&nbsp;Expo</Text>
+        </View>
         <Link href={"/city-details/6"}>Details With link</Link>
         <Link href={"/city-details/6"}>
-          <ThemedText>Details With link and Text inside</ThemedText>
+          <Text>Details With link and Text inside</Text>
         </Link>
-        <ThemedText onPress={() => router.navigate("/city-details/8")}>
+        <Text onPress={() => router.navigate("/city-details/8")}>
           Details with text with onPress
-        </ThemedText>
+        </Text>
 
         <Link
           href={{
@@ -54,7 +27,7 @@ export default function HomeScreen() {
         >
           Details With link with props
         </Link>
-        <ThemedText
+        <Text
           onPress={() =>
             router.navigate({
               pathname: "/(protected)/city-details/[id]",
@@ -63,44 +36,8 @@ export default function HomeScreen() {
           }
         >
           Details with text with onPress with props
-        </ThemedText>
+        </Text>
       </SafeAreaView>
-    </ThemedView>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: "center",
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: "center",
-  },
-  code: {
-    textTransform: "uppercase",
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: "stretch",
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
--- src/app/(protected)/city-details/[id].tsx
+++ src/app/(protected)/city-details/[id].tsx
@@ -1,14 +1,11 @@
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
 import { useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function CityDetails() {
   const { id } = useLocalSearchParams();
   return (
-    <ThemedView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <ThemedText>Esse foi o ID passado === {id}</ThemedText>
-    </ThemedView>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Esse foi o ID passado === {id}</Text>
+    </View>
   );
 }
