db4453ee2c3a445c20379825c482df98cc265933
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-14 10:32:29 -0300

    fix: adjust ImageBackground style in CityCard component for proper rendering

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -7,7 +7,10 @@ type CityCardProps = {
 
 export function CityCard({ cityPreview }: CityCardProps) {
   return (
-    <ImageBackground source={cityPreview.coverImage}>
+    <ImageBackground
+      source={cityPreview.coverImage}
+      style={{ width: 200, height: 200 }}
+    >
       <Text>{cityPreview.name}</Text>
       <Text>{cityPreview.country}</Text>
     </ImageBackground>
