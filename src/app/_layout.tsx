5e61303a48dfdcdf1040e74c8c8dd6a6aee110a2
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-15 10:40:33 -0300

    feat: implement font loading for Poppins font family in RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 
@@ -6,6 +7,31 @@ import { ThemeProvider } from "@shopify/restyle";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    PoppinsBlack: require("../../assets/fonts/Poppins-Black.ttf"),
+    PoppinsBlackItalic: require("../../assets/fonts/Poppins-BlackItalic.ttf"),
+    PoppinsBold: require("../../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsBoldItalic: require("../../assets/fonts/Poppins-BoldItalic.ttf"),
+    PoppinsExtraBold: require("../../assets/fonts/Poppins-ExtraBold.ttf"),
+    PoppinsExtraBoldItalic: require("../../assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    PoppinsExtraLight: require("../../assets/fonts/Poppins-ExtraLight.ttf"),
+    PoppinsExtraLightItalic: require("../../assets/fonts/Poppins-ExtraLightItalic.ttf"),
+    PoppinsItalic: require("../../assets/fonts/Poppins-Italic.ttf"),
+    PoppinsLight: require("../../assets/fonts/Poppins-Light.ttf"),
+    PoppinsLightItalic: require("../../assets/fonts/Poppins-LightItalic.ttf"),
+    PoppinsMedium: require("../../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsMediumItalic: require("../../assets/fonts/Poppins-MediumItalic.ttf"),
+    PoppinsRegular: require("../../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsSemiBold: require("../../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsSemiBoldItalic: require("../../assets/fonts/Poppins-SemiBoldItalic.ttf"),
+    PoppinsThin: require("../../assets/fonts/Poppins-Thin.ttf"),
+    PoppinsThinItalic: require("../../assets/fonts/Poppins-ThinItalic.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
   return (
     <ThemeProvider theme={theme}>
       <Stack>
