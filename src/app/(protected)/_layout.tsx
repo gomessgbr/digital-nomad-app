7fc1c81e364fec1b53764fd1f2a4fe27606824b0
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-25 17:25:10 -0300

    feat: remove title header from root stack

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -8,7 +8,12 @@ export default function ProtectedLayout() {
   }
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
