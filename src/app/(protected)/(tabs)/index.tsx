24a6ba9d017ff2a7f78b669629bc020d37977872
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-25 17:31:51 -0300

    feat: enhance HomeScreen with FlatList optimizations and scrolling support

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,17 +1,30 @@
 import { CityCard } from "@/components/CityCard";
 import { Screen } from "@/components/Screen";
 import { cityPreviewList } from "@/data/cities";
+import { useAppTheme } from "@/theme/useAppTheme";
 import { CityPreview } from "@/types";
+import { useScrollToTop } from "@react-navigation/native";
+import { useRef } from "react";
 import { FlatList, ListRenderItemInfo } from "react-native";
 
 export default function HomeScreen() {
+  const { spacing } = useAppTheme();
+  const flatListRef = useRef(null);
+  useScrollToTop(flatListRef);
   function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
     return <CityCard cityPreview={item} />;
   }
 
   return (
     <Screen>
-      <FlatList data={cityPreviewList} renderItem={renderItem} />
+      <FlatList
+        ref={flatListRef}
+        contentContainerStyle={{ gap: spacing.padding }}
+        data={cityPreviewList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
     </Screen>
   );
 }
