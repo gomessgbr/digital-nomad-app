8edd9ebe72dfaaf7485f1b23db98cad815234de2
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-26 06:59:45 -0300

    feat: CityCard component layout and improve styling

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,18 +1,31 @@
+import { useAppTheme } from "@/theme/useAppTheme";
 import { CityPreview } from "@/types";
-import { ImageBackground, Text } from "react-native";
+import { ImageBackground } from "react-native";
+import { Box } from "./Box";
+import { Icon } from "./Icon";
+import Text from "./Text";
 
 type CityCardProps = {
   cityPreview: CityPreview;
 };
 
 export function CityCard({ cityPreview }: CityCardProps) {
+  const { borderRadii } = useAppTheme();
   return (
     <ImageBackground
       source={cityPreview.coverImage}
       style={{ width: "100%", height: 280 }}
+      imageStyle={{ borderRadius: borderRadii.default }}
     >
-      <Text>{cityPreview.name}</Text>
-      <Text>{cityPreview.country}</Text>
+      <Box flex={1} padding="s24" justifyContent="space-between">
+        <Box alignSelf="flex-end">
+          <Icon name="Favorite-outline" color="text" />
+        </Box>
+        <Box>
+          <Text variant="title22">{cityPreview.name}</Text>
+          <Text variant="text16">{cityPreview.country}</Text>
+        </Box>
+      </Box>
     </ImageBackground>
   );
 }
