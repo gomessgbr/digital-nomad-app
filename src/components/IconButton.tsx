b82912f8198c71013c5d26a0b2aedde082bbd4d2
Gabriel Gomes <gabrielgomes98@gmail.com>
2026-05-26 10:04:06 -0300

    feat: new icon button component

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,29 @@
+import { useAppTheme } from "@/theme/useAppTheme";
+import { Pressable, PressableProps } from "react-native";
+import { Box } from "./Box";
+import { Icon, IconName } from "./Icon";
+
+type IconButtonProps = {
+  iconName: IconName;
+  onPress: PressableProps["onPress"];
+};
+
+export function IconButton({ iconName, onPress }: IconButtonProps) {
+  const { boxShadows } = useAppTheme();
+
+  return (
+    <Pressable onPress={onPress}>
+      <Box
+        backgroundColor="primary"
+        width={50}
+        height={50}
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="rounded"
+        style={{ boxShadow: boxShadows.primary }}
+      >
+        <Icon name={iconName} />
+      </Box>
+    </Pressable>
+  );
+}
